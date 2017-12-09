bf7696eec254ab3d2d451bc1c4cb3eeca7654d2c
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-09 23:29:37 +0900

    ref 1kai

--- Source_code/11kai/11-1.c
+++ Source_code/11kai/11-1.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 int main(void){
   int i,n,x;
-  printf("解法アルゴリズム１：再帰的なアルゴリズム\n");
-  printf("F(n)までを求める。\nn=");
+  printf("F(n)までを求めるn=");
   scanf("%d",&n);
   for(i=0;i<=n;i++){
     x=F(i);
     printf("F(%d)=%d\n",i,x);
-
+  }
 }
 
-}
-  int F(int n){
-  if((n==0)||(n==1)){
+int F(int n){
+  if((n==0 || n==1)){
     return 1;
-  }else if(n>=2){
-      return F(n-1)+F(n-2);
-
-    }else{
+  }else if(n >=2){
+    return F(n-1) + F(n-2);
+  }else{
     return -1;
-  }}
+  }
+}
