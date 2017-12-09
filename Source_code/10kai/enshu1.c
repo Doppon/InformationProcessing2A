d78ec94bca5ab26140bcf0eb35ad9ec75a1d715b
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-09 23:25:05 +0900

    ref 10kai

--- Source_code/10kai/enshu1.c
+++ Source_code/10kai/enshu1.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 int main(void){
   int n,m,i,max,a,b,t;
-
   printf("mの数値を入力してください:");
   scanf("%d",&m);
   printf("nの数値を入力してください:");
   scanf("%d",&n);
   printf("\n");
-
   if(n>m){
     max=m;
   }else{
     max=n;
   }
-
   for(i=1;i<=max;i++){
     a=m%i;
     b=n%i;
