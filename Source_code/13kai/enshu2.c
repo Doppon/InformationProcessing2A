40b7e48a42ed78e7671de53f2eb12bcc5cef0e2e
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-20 14:08:37 +0900

    ref 12kai 13kai

--- Source_code/13kai/enshu2.c
+++ Source_code/13kai/enshu2.c
@@ -9,64 +9,58 @@ cell D[n+1][WMAX+1];
 int main(void){
   int i,j,k,q,x,g,Vmax=0,t_max[n+1];
   for(j=0;j<=C;j++){
-    D[0][j].v=0;//�����
+    D[0][j].v=0;
     for(k=0;k<=n+1;k++){
-      D[0][j].t[k]=0;//t[0]~t[n+1]=0,����t[0]�Ȥ�ʤ�����k=1�Ǥ褯��?
+      D[0][j].t[k]=0;
     }
   }
-  for(i=1;i<=n;i++){//D[0][x]�ϼ�����v=0,t={��}
+  for(i=1;i<=n;i++){
     for(j=0;j<=C;j++){
       if(j<w[i]){
-				D[i][j].v=D[i-1][j].v;//���ԡ�[i-1?
+				D[i][j].v=D[i-1][j].v;
 					for(k=1;k<=n+1;k++){
-	  				D[i][j].t[k]=D[i-1][j].t[k];//���ԡ�t[k-1?
+	  				D[i][j].t[k]=D[i-1][j].t[k];
 					}
-      }else{//j>w
-			//��ʪi��ä�������ǲ��ͤ��⤤������
+      }else{
 				if(D[i-1][j-w[i]].v+v[i]>D[i-1][j].v){//w[i]
 	  			D[i][j].v=D[i-1][j-w[i]].v+v[i];//w[i]
 	  			for(k=1;k<=n;k++){
 	    			D[i][j].t[k]=D[i-1][j-w[i]].t[k];
 	  			}
-	  			D[i][j].t[i]=1;//Ƚ��
+	  			D[i][j].t[i]=1;
 	  			if(D[i][j].v>Vmax){
 	    			Vmax=D[i][j].v;
-	    			for(k=1;k<=n;k++){//1-n�ޤ�(t[5]��6����
+	    			for(k=1;k<=n;k++){
 	      			t_max[k]=D[i][j].t[k];
 	    			}
 	  			}
 				}else{
-				//���ͤ���ޤ�ʤ����
-	  		D[i][j].v=D[i-1][j].v;
-	  		for(k=1;k<=n;k++){
-	    	D[i][j].t[k]=D[i-1][j].t[k];
-}
-	}}//����
+					D[i][j].v=D[i-1][j].v;
+					for(k=1;k<=n;k++){
+					D[i][j].t[k]=D[i-1][j].t[k];
+					}
+				}
+			}
       printf("[v=%d,T=",D[i][j].v);
       printf("{");q=0;
-	for(g=1;g<=n;g++){
-	  if(D[i][j].t[g]==1){
+			for(g=1;g<=n;g++){
+	  		if(D[i][j].t[g]==1){
 	      q=1; 
-	    printf("%d,",g);
-             }
+	    	printf("%d,",g);
+        }
+			}
+			if(q!=1){printf(" ");}
+			printf("\b}]\n");
+  	}
+  printf("\n");
 	}
-		if(q!=1){printf(" ");}
-	printf("\b}]");
-
-
-      
-      }
-    printf("\n");
+printf("\n\n最適解:[v=%d:T={",Vmax);
+x=0;   
+for(x=1;x<=n+1;x++){
+	if(t_max[x]==1){  
+	  printf("%d,",x);
   }
-  printf("\n\n��Ŭ���[v=%d:T={",Vmax);
-  x=0;
-   
-	for(x=1;x<=n+1;x++){
-	  if(t_max[x]==1){
-	  
-	    printf("%d,",x);
-             }
-	}
-	printf("\b}]\n");
-	return 0;
+}
+printf("\b}]\n");
+return 0;
 }
