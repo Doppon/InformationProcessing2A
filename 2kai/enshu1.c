8c8e54c069ac8a3f805a31824d1534598f0516a3
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-15 00:14:44 +0900

    見逃していたら部分を再度リファクタリング

--- 2kai/enshu1.c
+++ 2kai/enshu1.c
@@ -5,7 +5,6 @@ int heap[HEAPSIZE];
 #define data_number 10
 static int data[data_number]={58,12,39,90,49,26,68,47,15,39};
 void pushheap(int *,int *,int);
-
 void pushheap(int *heap,int *n,int x){
   int i,j;
   if(++(*n)>=HEAPSIZE){
@@ -25,11 +24,6 @@ void pushheap(int *heap,int *n,int x){
   }
 }
 
-
-
-
-
-
 int main(void){
   int i=0,number=0;
   printf("--- データをヒープに格納 ---\n");
@@ -38,7 +32,6 @@ int main(void){
   for(i=0;i<data_number;i++){
     printf("in =%2d",data[i]);
     pushheap(heap,&number,data[i]);
-      printheap(heap,number);
+    printheap(heap,number);
   }
-
 }
